e2997f3a4143122e5ba81b5190f98db4fd0d95fb
Stephen Planck <splanck9271@gmail.com>
2025-06-10 13:40:38 -0400

    Add header comment for line editor

--- src/lineedit.h
+++ src/lineedit.h
@@ -1,3 +1,11 @@
+/*
+ * Interactive line editor interface.
+ *
+ * `line_edit` displays PROMPT and allows the user to edit the input using
+ * arrow keys, history search and other familiar shortcuts.  The completed
+ * command line is returned as a newly allocated string, which the caller
+ * must free when no longer needed.
+ */
 #ifndef LINEEDIT_H
 #define LINEEDIT_H
 
